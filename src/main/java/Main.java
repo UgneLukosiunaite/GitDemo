74964bd9c49eee2f51f61aaff40f357f654f9625
Ugne <u.lukosiunaite@gmail.com>
2022-10-06 14:44:43 +0300

    Add main class into test/java/

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import com.beust.jcommander.IStringConverter;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main (String[] args){
+
+        DayOfWeek actualWeekDay = LocalDate.now().getDayOfWeek();
+        System.out.println();
+
+    }
+
+}
